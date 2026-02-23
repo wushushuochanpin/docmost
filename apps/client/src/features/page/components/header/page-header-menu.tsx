6e37cbaa4dac8af465e77a00f64d34d6d828ea9c
zjunxu <zjunxu1989@gmail.com>
2026-02-23 11:47:01 +0800

    refactor: move editor font size control to page header toolbar

--- apps/client/src/features/page/components/header/page-header-menu.tsx
+++ apps/client/src/features/page/components/header/page-header-menu.tsx
@@ -37,6 +37,7 @@ import {
 } from "@/features/editor/atoms/editor-atoms.ts";
 import { formattedDate } from "@/lib/time.ts";
 import { PageStateSegmentedControl } from "@/features/user/components/page-state-pref.tsx";
+import { EditorFontSizeSegmentedControl } from "@/features/user/components/editor-font-size-pref";
 import MovePageModal from "@/features/page/components/move-page-modal.tsx";
 import { useTimeAgo } from "@/hooks/use-time-ago.tsx";
 import ShareModal from "@/features/share/components/share-modal.tsx";
@@ -74,6 +75,7 @@ export default function PageHeaderMenu({ readOnly }: PageHeaderMenuProps) {
       <ConnectionWarning />
 
       {!readOnly && <PageStateSegmentedControl size="xs" />}
+      <EditorFontSizeSegmentedControl size="xs" />
 
       <ShareModal readOnly={readOnly} />
 
